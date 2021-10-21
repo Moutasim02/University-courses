7db2bb543191e2989ade542772a695d3edcd9bbf
Moutasim02 <23410040+Moutasim02@users.noreply.github.com>
2021-10-21 21:16:57 +0400

    Added Chapter 3 description to README.md

--- src/com/structured_programming_SWE201/Chapter1and2/Exercising/CuTime.java
+++ src/com/structured_programming_SWE201/Chapter1and2/Exercising/CuTime.java
@@ -2,6 +2,12 @@ package com.structured_programming_SWE201.Chapter1and2.Exercising;
 
 public class CuTime {
     public static void main(String[] args) {
+    int i=1, j=2, k=3;
 
+    if (i>j)
+        if (i > k)
+            System.out.println("A");
+        else
+            System.out.println("B");
     }
 }
