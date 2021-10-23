a50ac0a80c324feb395434c581446c8631b02cfa
Moutasim02 <23410040+Moutasim02@users.noreply.github.com>
2021-10-23 20:43:53 +0400

    Delete src/com/structured_programming_SWE201/Chapter5/Assignment2 directory

--- src/com/structured_programming_SWE201/Chapter5/Assignment2/ProblemOne.java
+++
@@ -1,9 +0,0 @@
-package com.structured_programming_SWE201.Chapter5.Assignment2;
-
-import java.util.Scanner;
-
-public class ProblemOne {
-    Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt();
-
-}
