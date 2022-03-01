b86c32c2204daa9323a41279e6832bf6cf2e74e9
Moutasim02 <moutasim9997@gmail.com>
2022-03-01 21:31:12 +0400

    Added CSC202

---
+++ src/com/programming_II_CSC202/Lectures/ChapterNine/Rectangle.java
@@ -0,0 +1,13 @@
+package com.programming_II_CSC202.Lectures.ChapterNine;
+
+public class Rectangle {
+
+    //properties ---> attributes
+    double length, width;
+    
+
+    public double getArea(){
+        double area = length * width;
+        return area;
+    }
+}
---
+++ src/com/programming_II_CSC202/Lectures/ChapterNine/testRectangle.java
@@ -0,0 +1,11 @@
+package com.programming_II_CSC202.Lectures.ChapterNine;
+
+
+public class testRectangle {
+    public static void main(String[] args){
+        Rectangle r1 = new Rectangle();
+        r1.length = 10;
+        r1.width = 5.6;
+        System.out.println(r1.getArea());
+    }
+}
