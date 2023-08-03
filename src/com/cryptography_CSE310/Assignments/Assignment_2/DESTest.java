450f147df055fc973e4fc78bdef01eeb4dfb1127
Moutasim02 <moutasim9997@gmail.com>
2023-08-03 05:37:38 +0400

    Assignment 2 Added test file

---
+++ src/com/cryptography_CSE310/Assignments/Assignment_2/DESTest.java
@@ -0,0 +1,10 @@
+package com.cryptography_CSE310.Assignments.Assignment_2;
+
+public class DESTest {
+    public static void main(String[] args) {
+        String plaintext = "100010010101"; // 12 bits
+        String key = "110110101"; // 9 bits
+        // 5 rounds
+        // sub-keys -> k1=11010011, k2=00111100, k3=11110011, k4=10100011, k5=10111111
+    }
+}
