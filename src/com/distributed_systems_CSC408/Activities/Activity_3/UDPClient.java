ef3c97d5f60bca3c6ba96251dd8518ee2861b4d1
moutasim02 <moutasim9997@gmail.com>
2024-03-03 16:05:59 +0400

    Fix UDP Server/ Client

--- src/com/distributed_systems_CSC408/Activities/Activity_3/UDPClient.java
+++ src/com/distributed_systems_CSC408/Activities/Activity_3/UDPClient.java
@@ -5,21 +5,23 @@ import java.io.*;
 
 public class UDPClient{    
 
-    public static void main(String args[]) {  
-    // args[0] = message to be sent to the server; 
+    public static void main(String[] args) {
+    // args[0] = message to be sent to the server;
     // args[1] = IP address of the server
-
-        ...
-        try {
-            byte [] m = args[0].getBytes();
+        try (DatagramSocket aSocket = new DatagramSocket()) {
+            byte[] m = args[0].getBytes();
             InetAddress serverAddress = InetAddress.getByName(args[1]);
-            int serverPort = 20000;
+            int serverPort = 3000;
+            DatagramPacket request = new DatagramPacket(m, args[0].length(), serverAddress, serverPort);
+            aSocket.send(request);
             byte[] buffer = new byte[1000];
+            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+            aSocket.receive(reply);
             System.out.println("Received Reply: " + new String(reply.getData(), 0, reply.getLength()));
-        }catch (SocketException e){System.out.println("Error Socket: " + e.getMessage());
-        }catch (IOException e){System.out.println("Error IO: " + e.getMessage());
-        }finally { 
-            if(aSocket != null) aSocket.close();
+        } catch (SocketException e) {
+            System.out.println("Error Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error IO: " + e.getMessage());
         }
     }
 }
--- src/com/distributed_systems_CSC408/Activities/Activity_3/UDPServer.java
+++ src/com/distributed_systems_CSC408/Activities/Activity_3/UDPServer.java
@@ -3,17 +3,25 @@ package com.distributed_systems_CSC408.Activities.Activity_3;
 import java.net.*;
 import java.io.*;
 
-public class UDPServer{    
-   public static void main(String args[]) {     	
-    try{
-	    byte[] buffer = new byte[1000];
-	   	System.out.println("Server is ready and accepting clients' requests ... ");
-		while(true){ 				
-		}
- 	}catch (SocketException e){System.out.println("Error Socket: " + e.getMessage());
- 	}catch (IOException e) {System.out.println("Error IO: " + e.getMessage());
-	}finally {
-		if(aSocket != null) aSocket.close();
-	}
-   }
+public class UDPServer {
+    public static void main(String[] args) {
+        DatagramSocket aSocket = null;
+        try {
+            aSocket = new DatagramSocket(3000);
+            byte[] buffer = new byte[1000];
+            System.out.println("Server is ready and accepting clients' requests ... ");
+            while (true) {
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                aSocket.receive(request);
+                DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(), request.getPort());
+                aSocket.send(reply);
+            }
+        } catch (SocketException e) {
+            System.out.println("Error Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error IO: " + e.getMessage());
+        } finally {
+            if (aSocket != null) aSocket.close();
+        }
+    }
 }
